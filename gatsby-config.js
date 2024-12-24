ef8687a5acd50c1ea02ed0bf1946498b56b5ee8d
sal-digtan <salman.digtan@gmail.com>
2024-12-24 18:16:45 +0500

    changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@
 module.exports = {
   siteMetadata: {
     title: `test_gatsby_app`,
-    siteUrl: `https://cms-wp-react.netlify.app`
+    siteUrl: `http://localhost/gatsby-test-app/cms-wp/wordpress-6.7.1/wordpress/`
   },
   plugins: ["gatsby-plugin-netlify-cms"]
 };
